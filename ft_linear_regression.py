732c12d2b6bb3ef83dced8090132153e15864907
gclement42 <clement.games125@gmail.com>
2024-08-18 11:01:11 +0200

    feat: change calc for derivative

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -7,6 +7,8 @@ class LinearRegression:
         self.learning_rate = learning_rate
         self.a = a
         self.b = b
+        self.prev_a = 0
+        self.prev_b = 0
         self.x = np.array([int(row['features']) for row in data])
         self.y = np.array([int(row['target']) for row in data])
         self.m = len(data)
@@ -14,27 +16,35 @@ class LinearRegression:
         fig, self.ax = plt.subplots()
 
     def cost_function(self, predictions):
-        error = np.mean(predictions - self.y)
-        cost = 1 / (2 * self.m) * np.mean(error ** 2)
+        errors = self.calc_errors(predictions)
+        cost = (1 / (2 * self.m)) * np.sum(np.power(errors, 2))
         return cost
     
     def derivative(self):
-        predictions = self.calc_predictions()
-        errors = np.mean(predictions - self.y)
-        d_a = (1 / self.m) * np.mean(np.multiply(errors, self.x))
-        d_b = (1 / self.m) * np.mean(errors)
-        print(f'd_a: {d_a}, d_b: {d_b}')
+        d_a = float(0)
+        d_b = float(0)
+        for i in range(0, self.m):
+            d_a += float(((self.prev_a + self.prev_b * self.x[i]) - self.y[i]))
+            d_b += (((self.prev_a + self.prev_b * self.x[i]) - self.y[i]) * self.x[i])
+        d_a = (1 / self.m) * d_a
+        d_b = (1 / self.m) * d_b
         return d_a, d_b
     
     def gradient_descent(self):
-        predictions = self.calc_predictions()
-        errors = np.mean(predictions - self.y)
-        self.a = self.a - self.learning_rate * np.multiply((1 / self.m), np.sum(errors))
-        self.b = self.b - self.learning_rate * np.multiply((1 / self.m), np.mean(np.multiply(errors, self.x)))
+        d_a, d_b = self.derivative()
+        self.a = self.prev_a - (self.learning_rate * d_a)
+        self.b = self.prev_b - (self.learning_rate * d_b)
+        self.prev_a = self.a
+        self.prev_b = self.b
 
     def calc_predictions(self):
-        predictions = np.multiply(self.a, np.mean(self.x)) + self.b
+        predictions = self.b + (self.a * self.x)
         return predictions
+    
+    def calc_errors(self, predictions):
+        errors = predictions - self.y
+        return errors
+
     
     def train(self):
         cost = 11
@@ -57,8 +67,8 @@ class LinearRegression:
         self.ax.clear()
         
         plt.scatter(self.x, self.y)
-        x_values = np.linspace(min(self.x), max(self.x), self.m)
-        y_values = self.a * x_values + self.b
+        x_values = np.linspace(min(self.x), max(self.x), 100)
+        y_values = self.a + self.b * x_values
         self.line = plt.plot(x_values, y_values, color='red')
         plt.ylim(0, max(self.y))
         plt.xlabel('km')
@@ -67,7 +77,7 @@ class LinearRegression:
         plt.draw()
         plt.pause(0.1)
 
-model = LinearRegression(0.00001, 0, 0, utils.get_data())
+model = LinearRegression(0.0001, 0, 0, utils.get_data())
 model.train()
 
 
